a26853360f0d998d42dc96feca4b2c5104698fde
LukeStorry <luke.storry@gmail.com>
2014-12-08 10:31:32 +0000

    added digit-checking function

---
+++ MasterMind.hs
@@ -0,0 +1,9 @@
+digits :: String -> Bool
+digits [x:s] -- splits inputted string into first char and a string
+    | elems x [0..9]    = digits s --checks char, then recursively calls all others
+        | otherwise         = false --if any char not in 0-9, then false returned
+
+
+
+
+
