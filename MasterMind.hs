fcf8b9aa0594628b8252e1042af07f270eca2491
LukeStorry <luke.storry@gmail.com>
2014-12-08 10:49:43 +0000

    fixed bugs causing compiler errors

--- MasterMind.hs
+++ MasterMind.hs
@@ -1,15 +1,17 @@
+import Data.Bool
+import Prelude
 
 --Checks whether a string consists of digits.
 digits :: String -> Bool
 digits [x:s] -- splits inputted string into first char and a string
-    | elems x [0..9]    = digits s --checks char, then recursively calls all others
-        | otherwise         = false --if any char not in 0-9, then false returned
+    | elem s x [0..9]   = digits s --checks char, then recursively calls all others
+    | otherwise		= False --if any char not in 0-9, then false returned
 
 --checks whether the input is valid
 valid :: Int -> String -> Bool
 valid n guess 
-    | digits guess && length guess == 4	    = true
-    | otherwise				    = false
+    | digits guess && length guess == 4	    = True
+    | otherwise				    = False
 
 
 
